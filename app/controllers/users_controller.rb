7673cf0ea64a8b5aa55573d73c38333ceb72e8a1
EC2 Default User <ec2-user@ip-172-31-22-148.us-east-2.compute.internal>
2018-09-22 03:49:33 +0000

    勤怠表示画面退社ボタンまで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 require "date"
 class UsersController < ApplicationController
+  include UsersHelper
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
@@ -12,23 +13,24 @@ class UsersController < ApplicationController
     @users = User.all.paginate(page: params[:page])
   end
   
+  
   def show
     
     @user = User.find(params[:id])
-    if logged_in?
-      @user = current_user
-    end
+    # if logged_in?
+    #   @user = current_user
+    # end
     
     if current_user.admin? || current_user.id == @user.id
       # 曜日表示用に使用する
       @week = %w{日 月 火 水 木 金 土}
 
-      @date = DateTime.current
-      # # 今月
-      # @yearmonth = @date.strftime("%Y年%m月")
-      # 先月と来月
-      # @perv_month = @date.prev_month.strftime("%Y年%m月")
-      # @next_month = @date.next_month.strftime("%Y年%m月")
+      # @date = DateTime.current
+  #     # # 今月
+  #     # @yearmonth = @date.strftime("%Y年%m月")
+  #     # 先月と来月
+  #     # @perv_month = @date.prev_month.strftime("%Y年%m月")
+  #     # @next_month = @date.next_month.strftime("%Y年%m月")
      
       # 既に表示月があれば、表示月を取得する
       if not params[:first_day].nil?
@@ -39,11 +41,6 @@ class UsersController < ApplicationController
       end
       #最終日を取得する
       @last_day = @first_day.end_of_month
-    
-      
-       # 当月を昇順で取得し@daysへ代入
-      @days = @user.attendances.where('attendance_day >= ? and attendance_day <= ?', \
-      @first_day, @last_day).order('attendance_day')
       
       # 取得月の初日から終日まで繰り返し処理
       (@first_day..@last_day).each do |day|
@@ -56,45 +53,46 @@ class UsersController < ApplicationController
       end
     
     
-      # 当月を昇順で取得し@daysへ代入
+      # 当月を昇順で取得し@daysへ代入　= @first_day =< >= @last_day
         @days = @user.attendances.where('attendance_day >= ? and attendance_day <= ?', \
         @first_day, @last_day).order('attendance_day')
-        
       # 在社時間の集計、ついでに出勤日数も
-      i = 0
-      @days.each do |d|
-        if started_time.present? && d.finished_time.present?
-          second = 0
-          second = times(d.started_time,d.finished_time)
-          @total_time = @total_time.to_i + second.to_i
-          i = i + 1
-        end
-      end
+  #     i = 0
+  #     @days.each do |d|
+  #       if started_time.present? && d.finished_time.present?
+  #         second = 0
+  #         second = times(d.started_time,d.finished_time)
+  #         @total_time = @total_time.to_i + second.to_i
+  #         i = i + 1
+  #       end
+      # end
      
-      # 出勤日数、どっち使ってもOK
-      @attendances_count = i
-      @attendances_sum = @days.where.not(started_time: nil, finished_time: nil).count
+  #     # 出勤日数、どっち使ってもOK
+  #     @attendances_count = i
+  #     @attendances_sum = @days.where.not(started_time: nil, finished_time: nil).count
     else
     flash[:warning] = "他のユーザーの勤怠情報は閲覧できません。"
     redirect_to current_user
     end
   end
   
+  # 出勤ボタン
   def started_time
     @user = User.find(params[:id])
     @started_time = @user.attendances.find_by(attendance_day: Date.current)
     @started_time.update_attributes(started_time: DateTime.new(DateTime.now.year,\
     DateTime.now.month, DateTime.now.day,DateTime.now.hour,DateTime.now.min,0))
     flash[:info] = "今日も１日元気に頑張りましょう！"
-    # redirect_to @user
+    redirect_to @user
   end
-
+  
+  # 退勤ボタン
   def finished_time
     @user = User.find(params[:id])
     @finished_time = @user.attendances.find_by(attendance_day: Date.current)
-    timeout = DateTime.new(DateTime.now.year,DateTime.now.month,\
+    finishedtime = DateTime.new(DateTime.now.year,DateTime.now.month,\
     DateTime.now.day,DateTime.now.hour,DateTime.now.min,0)
-    @finished_time.update_attributes(finished_time: timeout)
+    @finished_time.update_attributes(finished_time: finishedtime)
     flash[:info] = "今日も一日お疲れ様でした！"
     redirect_to @user
   end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,9 +2,9 @@ module UsersHelper
 
   # 在社時間計上用
   def working_hours(a,b)
-    timein = Time.mktime(a.year, a.month, a.day, a.hour, a.min, 0, 0)
-    timeout = Time.mktime(b.year, b.month, b.day, b.hour, b.min, 0, 0)
-    (((timeout - timein) / 60) / 60).truncate(2)
+    startedtime = Time.mktime(a.year, a.month, a.day, a.hour, a.min, 0, 0)
+    finishedtime = Time.mktime(b.year, b.month, b.day, b.hour, b.min, 0, 0)
+    (((finishedtime - startedtime) / 60) / 60).truncate(2)
   end
   
   # 引数の時刻データの秒を０にして差を求める
@@ -19,9 +19,18 @@ module UsersHelper
     format("%.2f", ((t.hour * 60.0) + t.min)/60) if !t.blank?
   end
   
-   
-  
-  
+  # 現在ログイン中のユーザーを返す (いる場合)
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
   
   # 引数で与えられたユーザーのGravatar画像を返す
   def gravatar_for(user, size: 80)
