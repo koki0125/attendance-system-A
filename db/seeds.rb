8c1a7d8ca0d80af5813ba571385590d87c520d14
EC2 Default User <ec2-user@ip-172-31-22-148.us-east-2.compute.internal>
2018-12-17 15:58:25 +0000

    fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,8 @@ User.create!(name:  "猫ひろし(一般ユーザー)",
              password_confirmation: "foobar",
              department: "WAHAHA本舗",
              admin:     false,
-             basic_time: Time.parse("7:30"),
-             specified_working_time: Time.parse("8:00")
+             basic_time: Time.zone.parse("7:30"),
+             specified_working_time: Time.zone.parse("8:00")
              )
 
 99.times do |n|
