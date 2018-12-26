d135b34e92652aa27c937f3d81634400e7024f6c
EC2 Default User <ec2-user@ip-172-31-45-155.us-east-2.compute.internal>
2018-12-26 23:44:47 +0000

    adjust update_bunch

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -40,30 +40,53 @@ class AttendancesController < ApplicationController
   def update_bunch
     @user = User.find(params[:id])
     
-    attendances_params.each do |id, item|
+    attendances_params.each do |id, time|
       attendance = Attendance.find(id)
       
       #当日以降の編集はadminユーザのみ
       if attendance.attendance_day > Date.current && !current_user.admin?
-        flash[:warning] = '明日以降の勤怠編集は出来ません。'
-      
-      elsif item["started_time"].blank? && item["started_time"].blank?
+    
+      elsif time["started_time"].blank? && time["finished_time"].blank?
 
       #出社時間と退社時間の両方の存在を確認
-      elsif item["started_time"].blank? || item["started_time"].blank?
+      elsif time["started_time"].blank? || time["finished_time"].blank?
         flash[:warning] = '一部編集が無効となった項目があります。'
       
       #出社時間 > 退社時間ではないか
-      elsif item["started_time"].to_s > item["finished_time"].to_s
+      elsif time["started_time"].to_s > time["finished_time"].to_s
         flash[:warning] = '出社時間より退社時間が早い項目がありました'
       
       else
-        attendance.update_attributes(item)
-        flash[:success] = '勤怠時間を更新しました。'
+        attendance.update_attributes(time)
+        flash[:success] = '勤怠時間を更新しました。なお本日以降の更新はできません。'
       end
     end #eachの締め
     redirect_to user_url(@user, params:{ id: @user.id, first_day: params[:first_day]})
   end
+  
+      # def update_bunch
+        
+      #   @user = User.find(params[:id])
+      #   attendances_params.each do |id, item|
+      #   attendance = Attendance.find(id)
+      #     #未来の情報は一般ユーザーは更新できないように設定（管理者のみ編集可能）
+      #     if attendance.attendance_day > Date.today && !current_user.admin?
+      #       raise
+              
+      #     #出社時間と退社時間の両方の存在を検証
+      #     elsif attendance.attendance_day!=Date.today && item["started_time"].blank? ^ item["finished_time"].blank?
+      #       flash[:warning] = '出社時間と退社時間は両方入力してください。'
+      #     #出社時間より退社時間が遅いことを検証
+      #     elsif item["started_time"].to_s > item["finished_time"].to_s
+      #       flash[:warning] = '出社時間＜退社時間となるように入力してください。'
+      #     else
+      #       attendance.update_attributes(item)
+      #       flash[:success] = "更新しました！なお本日以降の更新はできません。"
+      #     end
+      #   end
+      # #セレクトユーザーの編集した月ページへ
+      # redirect_to  user_url(@user, params:{ id: @user.id, first_day: params[:first_day]})
+      # end
 
 
   # プライベート
