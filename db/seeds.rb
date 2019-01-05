911b6792e6a0ca65e3f8182741ae2d31b0982ced
EC2 Default User <ec2-user@ip-172-31-45-155.us-east-2.compute.internal>
2019-01-05 10:15:49 +0000

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,40 +21,19 @@ User.create!(name:  "野原ひろし(上長ユーザー)",
              department: "春日部防衛隊総統室長",
              employee_number: 222,
              admin:     false,
-             superior:  true,
-             designated_start_time: Time.zone.parse("10:00"),
-             designated_finish_time: Time.zone.parse("18:00"),
-             uid: 222,
+             superior: true,
              basic_time: Time.zone.parse("7:30"),
             # specified_working_time: Time.zone.parse("8:00")
-             )    
-
-User.create!(name:  "猫ひろし(上長ユーザー)",
+             )             
+             
+User.create!(name:  "猫ひろし(一般ユーザー)",
              email: "example333@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
              department: "WAHAHA本舗",
              employee_number: 333,
              admin:     false,
-             superior:  true,
-             designated_start_time: Time.zone.parse("15:00"),
-             designated_finish_time: Time.zone.parse("23:00"),
-             uid: 333,
-             basic_time: Time.zone.parse("7:30"),
-            # specified_working_time: Time.zone.parse("8:00")
-             ) 
-             
-User.create!(name:  "五木ひろし(一般ユーザー)",
-             email: "example444@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             department: "WAHAHA本舗",
-             employee_number: 444,
-             admin:     false,
-             superior:  false,
-             designated_start_time: Time.zone.parse("10:00"),
-             designated_finish_time: Time.zone.parse("18:00"),
-             uid: 444,
+             superior: false,
              basic_time: Time.zone.parse("7:30"),
             # specified_working_time: Time.zone.parse("8:00")
              )
@@ -68,13 +47,9 @@ User.create!(name:  "五木ひろし(一般ユーザー)",
                email: email,
                password:              password,
                password_confirmation: password,
-               department: "WAHAHA本舗",
                employee_number: "#{n+1}.to_i",
                admin:     false,
-               superior:  false,
-               designated_start_time: Time.zone.parse("10:00"),
-               designated_finish_time: Time.zone.parse("18:00"),
-               uid: "#{n+1}.to_i",
+               superior: false,
                basic_time: Time.zone.parse("7:30"),
               # specified_working_time: Time.zone.parse("8:00")
              )
