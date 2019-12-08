19f8b7507f81cf75d2f24043703efbb085e52875
EC2 Default User <ec2-user@ip-172-31-45-155.us-east-2.compute.internal>
2019-12-08 01:37:36 +0000

    add yard to gem & display overtime_status

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'jquery-rails',               '4.3.1'
 gem 'turbolinks',                 '5.0.1'
 gem 'jbuilder',                   '2.7.0'
 gem 'bootstrap-sass',             '3.3.7'
+gem 'yard'
+gem 'yard-activesupport-concern'
 # for csv_import 
 gem 'activerecord-import'
 gem 'csv'
--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,5 +1,8 @@
 module AttendancesHelper
 
+# 残業時間の計算（残業時間 - 定時）
+# @param  str Time
+# @return str Time
   def calculate_overtime(over, basic)
     sec = Time.parse(over) - Time.parse(basic)
     p sec
@@ -10,4 +13,15 @@ module AttendancesHelper
     return Time.at(sec).utc.strftime("%R")
   end
   
+# 残業ステータスの表示分け
+# @param  int 0 ~ 3
+# @return str
+  def overtime_status(num)
+    case num
+      when 0 then return "なし"
+      when 1 then return "申請中"
+      when 2 then return "受理"
+      when 3 then return "却下"
+    end
+  end
 end
