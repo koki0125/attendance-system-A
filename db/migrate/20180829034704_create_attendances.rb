6c1bd772aa31ad3c828ab72458d78956136cc3a6
EC2 Default User <ec2-user@ip-172-31-45-155.us-east-2.compute.internal>
2020-02-03 23:18:17 +0000

    create bases_table

--- db/migrate/20180829034704_create_attendances.rb
+++ db/migrate/20180829034704_create_attendances.rb
@@ -1,10 +1,10 @@
 class CreateAttendances < ActiveRecord::Migration[5.1]
   def change
     create_table :attendances do |t|
-      t.references :user, foreign_key: true
-      t.datetime :started_time
-      t.datetime :finished_time
-      t.date :attendance_day
+      t.references  :user,          foreign_key: true
+      t.datetime    :started_time
+      t.datetime    :finished_time
+      t.date        :attendance_day
 
       t.timestamps
     end
