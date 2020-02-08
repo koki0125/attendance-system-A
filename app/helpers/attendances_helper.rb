172cf5a1de9c3feeb6603e0d1b62a1c2f00f3f7a
EC2 Default User <ec2-user@ip-172-31-45-155.us-east-2.compute.internal>
2020-02-08 12:22:23 +0000

    done for log

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,5 +1,6 @@
 module AttendancesHelper
   
+  # 
   def modified_working_hours(a,b)
     startedtime = Time.mktime(a.year, a.month, a.day, a.hour, a.min, 0, 0)
     finishedtime = Time.mktime(b.year, b.month, b.day, b.hour, b.min, 0, 0)
@@ -16,6 +17,24 @@ module AttendancesHelper
       return "-"+Time.at(sec).utc.strftime("%R")
     end
     return Time.at(sec).utc.strftime("%R")
+  end
+  
+  # 表示用
+  def fixed_started_time(d)
+    if d.modified_started_time.present?
+      return d.modified_started_time.strftime("%H:%M")
+    else
+      return d.started_time.strftime("%H:%M")
+    end 
+  end
+  
+  # 表示用
+  def fixed_finished_time(d)
+    if d.modified_finished_time.present?
+      return d.modified_finished_time.strftime("%H:%M")
+    else
+      return d.expected_finish_time.strftime("%H:%M")
+    end 
   end
   
 end
